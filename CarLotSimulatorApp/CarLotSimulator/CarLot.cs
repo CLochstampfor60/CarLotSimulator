9f621fa719aa33347c5f8e867692fe53249890d7
CLochstampfor60 <cflochjr@gmail.com>
2023-10-02 17:22:12 -0400

    assignment complete

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        //*************BONUS X 2*************//
+        //Create a CarLot class
+        //It should have at least one property: a List of cars
+
+        public CarLot() {
+
+        }
+        public List<Car> carList = new List<Car>();
+
+    }
+}
